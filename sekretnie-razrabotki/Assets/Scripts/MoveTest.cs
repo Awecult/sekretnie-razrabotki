36a8033240a28b93a04fc0206a1ceab4726e2ce8
Danil <kosykh.d2@gmail.com>
2023-08-04 16:01:15 +0700

    Po kaifu sdelal
    Чуть лучше всё стало но всё ещё говно

--- sekretnie-razrabotki/Assets/Scripts/MoveTest.cs
+++ sekretnie-razrabotki/Assets/Scripts/MoveTest.cs
@@ -9,20 +9,20 @@ public class MoveTest : MonoBehaviour
     public float speed;
     public float jumpheight;
     public Transform groundCheck;
-    public bool isGrounded;
+    public bool isGrounded = false;
     Animator anim;
 
     void Start()    
     {
         rb = GetComponent<Rigidbody2D>();
-        //groundCheck = GameObject.FindGameObjectWithTag("Ground").transform;
+        groundCheck = GameObject.FindGameObjectWithTag("Ground").transform;
     }
     void Update()
     {
         Flip();
-        //CheckGround();
+        CheckGround();
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space) /*&& isGrounded*/)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             rb.AddForce(transform.up * jumpheight, ForceMode2D.Impulse);
 
     }
@@ -33,9 +33,24 @@ public class MoveTest : MonoBehaviour
         if (Input.GetAxis("Horizontal") < 0)
         transform.localRotation = Quaternion.Euler(0, 180, 0);
     }
-    //void CheckGround()
-    //{
-        //Collider2D[] colliders = Physics2D.OverlapBoxAll(groundCheck.position, 1);
+    
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision) {
+        isGrounded = false;
+    }
+
+    void CheckGround()
+    {
+        /*if (OnCollisionStay2D == true) 
+        {
+            isGrounded
+        }*/
+        //Collider2D[] colliders = Physics2D.OverlapBoxAll(groundCheck.position, BoxSize, 0);
         //isGrounded = colliders.Length > 1;
-    //}
+    }
 }
